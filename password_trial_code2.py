7efddc79b1af7f5c3684a99f3a8a7897b38baa39
Hank Lee <hank7468@gmail.com>
2019-08-20 18:04:59 +0800

    whileloop version

---
+++ password_trial_code2.py
@@ -0,0 +1,11 @@
+# 不使用while true完成此程式
+sp = 'hank5134' # sp = set password
+x = 3 # x = trials left
+while x > 0:
+	x = x - 1
+	ip = input('please enter the password: ')
+	if ip == sp:
+		print('password is correct.')
+		break
+	else:
+		print('password is not correct.', 'trials remaining: ', x)
