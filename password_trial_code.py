95779bc288d65b88defdf468fe016ef02cd1670c
Hank Lee <hank7468@gmail.com>
2019-08-20 17:45:36 +0800

    version no.2

--- password_trial_code.py
+++ password_trial_code.py
@@ -1,16 +1,14 @@
 # password trial code
 sp = 'hank5134' # sp = set password
-x = 1 # x = number of tirals
+x = 3 # x = remaining trials
 while True:
 	ip = input('please enter the password: ') # ip = input password
 	if ip == sp:
 		print('password is correct.')
 		break
-	elif ip != sp and x < 4:
-		print('password is incorrect.')
-		y = 3 - x # remaining trials
-		if y == 0:
+	else:
+		x = x - 1
+		print('password is incorrect.', 'there are', x, 'times left.')
+		if x == 0:
 			print('account is locked because password is not correctly input mor than 3 times.')
 			break
-		x = x + 1
-		print('remaining trials: ', y)
